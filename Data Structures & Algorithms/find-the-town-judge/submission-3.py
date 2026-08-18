017736ccd23de7dc3083edd161e903c55cd0af95
Shafial Zaman <109210987+shafialzaman@users.noreply.github.com>
2026-08-17 20:03:11 -0700

    Add: find-the-town-judge - submission-3

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        inc = defaultdict(int)
+        out = defaultdict(int)
+
+        for i,j in trust:
+            inc[i] += 1
+            out[j] += 1
+
+        print(inc,out)
+        
+        temp = -1
+        for b in out:
+            if out[b] == n - 1:
+                temp = b
+            if out[b] == n - 1 and b not in inc:
+                return b
+
+        if temp not in inc:
+            return temp
+        # if temp not in inc:
+        #     return temp
+
+
+        return -1
